deb1405ddbd7d9d9d78b9a6d837d91617803d959
Phil <Phil@DESKTOP-K5RG0SE>
2016-10-22 12:06:38 -0600

    Small changes

--- Game/BlackJackConsoleApp/EntryPoint.cs
+++ Game/BlackJackConsoleApp/EntryPoint.cs
@@ -15,45 +15,84 @@ namespace BlackJackConsoleApp
     {
         static void Main(string[] args)
         {
-            Thread thread1 = new Thread(new ThreadStart(WriteGameResults));
-            Thread thread2 = new Thread(new ThreadStart(WriteGameResults));
-            Thread thread3 = new Thread(new ThreadStart(WriteGameResults));
-            Thread thread4 = new Thread(new ThreadStart(WriteGameResults));
-            Thread thread5 = new Thread(new ThreadStart(WriteGameResults));
-            Thread thread6 = new Thread(new ThreadStart(WriteGameResults));
-            Thread thread7 = new Thread(new ThreadStart(WriteGameResults));
-            Thread thread8 = new Thread(new ThreadStart(WriteGameResults));
+            Thread thread1 = new Thread(new ThreadStart(WriteGameResultsPhilsVersion));
+            //Thread thread2 = new Thread(new ThreadStart(WriteGameResults));
+            //Thread thread3 = new Thread(new ThreadStart(WriteGameResults));
+            //Thread thread4 = new Thread(new ThreadStart(WriteGameResults));
+            //Thread thread5 = new Thread(new ThreadStart(WriteGameResults));
+            //Thread thread6 = new Thread(new ThreadStart(WriteGameResults));
+            //Thread thread7 = new Thread(new ThreadStart(WriteGameResults));
+            //Thread thread8 = new Thread(new ThreadStart(WriteGameResults));
 
             thread1.Start();
-            thread2.Start();
-            thread3.Start();
-            thread4.Start();
-            thread5.Start();
-            thread6.Start();
-            thread7.Start();
-            thread8.Start();
+            //thread2.Start();
+            //thread3.Start();
+            //thread4.Start();
+            //thread5.Start();
+            //thread6.Start();
+            //thread7.Start();
+            //thread8.Start();
 
 
             Console.Read();
 
         }
 
-        private static void WriteGameResults()
+        private static void WriteGameResultsPhilsVersion()
         {
-            Player player1 = new Player(new ZenStrategy(), "Bob");
+           // Player player1 = new Player(new ZenStrategy(), "Bob");
             Player player2 = new Player(new UshtonStrategy(), "John");
-
-            Deck deck = new Deck(1);
-
-            IList<Player> players = new List<Player>();
-            players.Add(player1);
-            players.Add(player2);
-
-            BlackJackGame game = new BlackJackGame(1000000, players, deck, 60.0);
-
-            foreach (Player player in players)
+            // players.Add(player1);
+            // players.Add(player2);
+            
+            int numOfPlayers = 7;
+            int numOfRounds = 2;
+            while (numOfPlayers > 0)
             {
-                Console.WriteLine(player.name + ": " + player.Balance + " w/l:6 " + player.GetWinAmountFor(6) + " insurance w/l:6 " + player.GetInsuranceWinAmountFor(6));
+                List<Player> players = new List<Player>();
+                Deck deck = new Deck(1);
+                EntryPoint ep = new EntryPoint();
+                ep.createPlayers(numOfPlayers, players);
+                // Adjust rounds accordingly
+                if (numOfPlayers == 4)
+                {
+                    numOfRounds = 3;
+                }
+                if (numOfPlayers == 3)
+                {
+                    numOfRounds = 4;
+                }
+                if (numOfPlayers == 2)
+                {
+                    numOfRounds = 5;
+                }
+                if (numOfPlayers == 1)
+                {
+                    numOfRounds = 6;
+                }
+
+                BlackJackGame game = new BlackJackGame(numOfRounds, players, deck);
+                // Print round and player info
+                Console.WriteLine("\t" + "Number of Rounds: " + numOfRounds.ToString() + ", Number of Players: " + numOfPlayers.ToString());
+                // Print each player and their balance
+                foreach (Player player in players)
+                {
+                    Console.WriteLine(player.name + ": " + player.Balance);
+                }
+                numOfPlayers--;
+            }
+        }
+
+
+
+        // Creates a certain number of players
+        public void createPlayers(int numOfPlayers, List<Player> playersList)
+        {
+            while (numOfPlayers > 0)
+            {
+                Player player = new Player(new BasicStrategy(), "player:" + numOfPlayers.ToString());
+                playersList.Add(player);
+                numOfPlayers--;
             }
         }
     }
--- Game/Game/BlackJackGame.cs
+++ Game/Game/BlackJackGame.cs
@@ -13,6 +13,7 @@ namespace Game
     {
         private IList<Player> players;
         private int roundsToPlay;
+        public double deckPen;
 
         public BlackJackGame(int roundsToPlay, IList<Player> players, Deck deck, double penetrationPercent)
         {
@@ -118,6 +119,118 @@ namespace Game
                 dealer.EndRound(dealer.RoundValue, false);
 
 
+                //Adjust count for players
+                foreach (Player player in players)
+                {
+                    player.AdjustCount(cardsSeenThisRound);
+                }
+
+                roundsToPlay--;
+            }
+        }
+
+        // Constructor that that doesnt need a set penetration, instead updates penetration as game goes (actually havent implemented that yet)
+        public BlackJackGame(int roundsToPlay, IList<Player> players, Deck deck)
+        {
+            this.players = players;
+            this.roundsToPlay = roundsToPlay;
+
+            double initialDeckSize = deck.CardsLeft();
+
+            Dealer dealer = new Dealer(new DealerStrategy());
+            dealer.SetDeck(deck);
+
+            foreach (Player player in players)
+            {
+                player.SetDeck(deck);
+            }
+
+            while (roundsToPlay > 0)
+            {
+                // Check if we need to reshuffle
+                double currentCardsLeft = deck.CardsLeft();
+                if (currentCardsLeft == 0)
+                {
+                    deck.ShuffleDeck();
+                    foreach (Player player in players)
+                    {
+                        player.ResetCount();
+                    }
+                }
+
+                dealer.DoInitialDraw();
+                foreach (Player player in players)
+                {
+                    player.DoInitialDraw();
+                }
+
+                Card upCard = dealer.FaceUpCard;
+
+                if (dealer.CanTakeInsurance())
+                {
+                    bool hasBlackJack = dealer.HasBlackJack();
+
+                    foreach (Player player in players)
+                    {
+                        if (player.TakeInsurance(upCard)) // If player took insurance
+                        {
+                            player.AdjustBalanceFromInsuranceBet(hasBlackJack); // adjust balance based off if dealer had bj
+                        }
+                    }
+
+                    if (hasBlackJack) // Then end round
+                    {
+                        // Track cards that were seen
+                        IList<Card> cardsSeen = new List<Card>();
+                        foreach (Player player in players)
+                        {
+                            foreach (Card card in player.GetCurrentRoundCards())
+                            {
+                                cardsSeen.Add(card);
+                            }
+                        }
+                        foreach (Card card in dealer.GetCurrentRoundCards())
+                        {
+                            cardsSeen.Add(card);
+                        }
+
+                        //Adjust count for players
+                        foreach (Player player in players)
+                        {
+                            player.AdjustCount(cardsSeen);
+                            player.EndRound(dealer.RoundValue, hasBlackJack);
+                        }
+
+                        dealer.EndRound(dealer.RoundValue, hasBlackJack);
+                        roundsToPlay--;
+                        continue;
+                    }
+                }
+
+                // Play out round for dealer and player, and track cards seen
+                IList<Card> cardsSeenThisRound = new List<Card>();
+                foreach (Player player in players)
+                {
+                    player.PlayOutRound(upCard);
+                    foreach (Card card in player.GetCurrentRoundCards())
+                    {
+                        cardsSeenThisRound.Add(card);
+                    }
+                }
+                dealer.PlayOutRound(upCard);
+                foreach (Card card in dealer.GetCurrentRoundCards())
+                {
+                    cardsSeenThisRound.Add(card);
+                }
+
+                // Ends game for players and dealer
+                foreach (Player player in players)
+                {
+                    player.EndRound(dealer.RoundValue, false);
+                }
+
+                dealer.EndRound(dealer.RoundValue, false);
+                
                 //Adjust count for players
                 foreach (Player player in players)
                 {
